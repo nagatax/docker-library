b8dacca6d0f44dc14010fb8ae76cb756d11a3c0a
nagatax <4foview@gmail.com>
2022-04-23 16:51:03 +0900

    [gcc]バージョンを11.3.0に更新する

--- gcc/hooks/env.sh
+++ gcc/hooks/env.sh
@@ -13,11 +13,11 @@ export DOCKER_BUILDKIT=1
 # Installing version
 ## gcc
 export GCC="gcc"
-export GCC_VERSION="11.2.0"
+export GCC_VERSION="11.3.0"
 export GCC_PAKAGE="${GCC}-${GCC_VERSION}"
 export GCC_PAKAGE_FILE="${GCC_PAKAGE}.tar.gz"
 export GCC_URL="https://ftp.jaist.ac.jp/pub/GNU/${GCC}/${GCC_PAKAGE}/${GCC_PAKAGE_FILE}"
 
 # BUILD Setting
-export GCC_LATEST="11.2.0"
+export GCC_LATEST="11.3.0"
 export D_IMAGE=${GCC}:${GCC_VERSION}
