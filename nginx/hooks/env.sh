dfe714e5a8ba0689ca67e38a6a2d805eb167a770
nagatax <4foview@gmail.com>
2022-04-23 17:32:09 +0900

    [nginx]不要な処理の削除とコメントを修正する

--- nginx/hooks/env.sh
+++ nginx/hooks/env.sh
@@ -2,14 +2,6 @@
 
 ##################################################
 # 環境変数を定義する
-#
-# Environment Variables
-#  PACKAGE: package name
-#  PACKAGE_VERSION: package version
-#  PACKAGE_LATEST: latest version of package
-#  D_IMAGE: image name of Docker
-#  DH_IMAGE: image name of Docker Hub
-#  DH_LATEST: latest image name of Docker Hub
 ##################################################
 
 # Set shell option
@@ -25,7 +17,6 @@ export PCRE_VERSION="8.45"
 export PCRE_PAKAGE="${PCRE}-${PCRE_VERSION}"
 export PCRE_PAKAGE_FILE="${PCRE_PAKAGE}.tar.gz"
 export PCRE_URL="https://sourceforge.net/projects/pcre/files/${PCRE}/${PCRE_VERSION}/${PCRE_PAKAGE_FILE}/download"
-export PCRE_SIG_URL="https://sourceforge.net/projects/pcre/files/${PCRE}/${PCRE_VERSION}/${PCRE_PAKAGE_FILE}.sig/download"
 ## nginx
 export NGINX="nginx"
 export NGINX_VERSION="1.21.6"
