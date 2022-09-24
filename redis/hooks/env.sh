581ba27b598bb17ad30b57483932efc086384eb6
nagatax <4foview@gmail.com>
2022-09-24 13:51:49 +0900

    [redis]バージョンを7.0.5に更新する

--- redis/hooks/env.sh
+++ redis/hooks/env.sh
@@ -12,11 +12,11 @@ export DOCKER_BUILDKIT=1
 
 # Installing version
 export REDIS="redis"
-export REDIS_VERSION="6.2.6"
+export REDIS_VERSION="7.0.5"
 export REDIS_PAKAGE="${REDIS}-${REDIS_VERSION}"
 export REDIS_PAKAGE_FILE="${REDIS_PAKAGE}.tar.gz"
 export REDIS_URL="http://download.redis.io/releases/${REDIS_PAKAGE_FILE}"
 
 # BUILD Setting
-export REDIS_LATEST="6.2.6"
+export REDIS_LATEST="7.0.5"
 export D_IMAGE=${REDIS}:${REDIS_VERSION}
