b5769855b717771b33079b56ba60792bb3529e2b
nagatax <4foview@gmail.com>
2023-04-23 17:07:47 +0900

    [php]バージョンを8.2.5に更新する

--- php/hooks/env.sh
+++ php/hooks/env.sh
@@ -13,13 +13,13 @@ export DOCKER_BUILDKIT=1
 # Installing version
 ## php
 export PHP="php"
-export PHP_VERSION="8.1.10"
+export PHP_VERSION="8.2.5"
 export PHP_PAKAGE="${PHP}-${PHP_VERSION}"
 export PHP_PAKAGE_FILE="${PHP}-${PHP_VERSION}.tar.gz"
 export PHP_URL="https://www.php.net/distributions/${PHP_PAKAGE_FILE}"
-export PHP_SHA256="3ea4f323109dfbc8d2631d08aa0e08602c1f713678e9dc6c750f081ef49eab0f"
-export COMPOSER_SHA="55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae"
+export PHP_SHA256="8974dea2507155471660b13a0bcbdc165ac778eeb845a7dbd65d5ffb92738c0a"
+export COMPOSER_SHA="55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae" # see https://getcomposer.org/download/
 
 # BUILD Setting
-export PHP_LATEST="8.1.10"
+export PHP_LATEST="8.2.5"
 export D_IMAGE=${PHP}:${PHP_VERSION}
